d25e0090fca8405ca5acd47185b9bfa3f9009d75
ossang08 <68271074+ossang08@users.noreply.github.com>
2020-08-04 00:37:31 +0900

    change all korean into english

--- little_shark.cpp
+++ little_shark.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main() {
-	// input ���� ����
+	// input file connection
 	ifstream fin("C:\\Users\\USER\\Desktop\\input.txt");
 	if (!fin) {
 		cout << "err" << endl; return 0;
 	}
-	// field size �Է�
+	// get field size 
 	int f_size;
 	fin >> f_size;
-	// field ���� �� �ʱ�ȭ
+	// make and initialize field
 	int **field = new int*[f_size];
 	for (int i = 0; i < f_size; i++) {
 		field[i] = new int[f_size];
@@ -21,7 +21,7 @@ int main() {
 			fin >> field[i][j];
 		}
 	}
-	// field ���
+	// print field
 	for (int i = 0; i < f_size; i++) {
 		for (int j = 0; j < f_size; j++) {
 			cout << field[i][j] << ' ';
